5517dcf3d09a461079b47f336aa6a57a60ac3466
Leah1977 <lh020277@gmail.com>
2021-11-21 22:07:21 +0000

    Add toasts templates
    Add toast message for errors
    Add toast message for info
    Add toast message for warning

--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -15,12 +15,13 @@ def view_basket(request):
 def add_to_basket(request, item_id):
     """ Add a quantity of the chosen product to the basket """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, (pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     size = None
     if 'product_size' in request.POST:
-        basket = request.session.get('basket', {})
+        size = request.POST['product_size']
+    basket = request.session.get('basket', {})
 
     if size:
         if item_id in list(basket.keys()):
@@ -51,7 +52,8 @@ def add_to_basket(request, item_id):
 
 def adjust_basket(request, item_id):
     """ Adjust the quantity of the specified product to the basket """
-
+    
+    product = get_object_or_404(Product,(pk=item_id)
     quantity = int(request.POST.get('quantity'))
     size = None
     if 'product_size' in request.POST:
@@ -61,15 +63,22 @@ def adjust_basket(request, item_id):
     if size:
         if quantity > 0:
             basket[item_id]['items_by_size'][size] = quantity
+            messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {basket[item_id]["items_by_size"][size]}')
+     
         else:
             del basket[item_id]['items_by_size'][size]
             if not basket[item_id]['items_by_size']:
                 basket.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {product.name} from your basket')
     else:
         if quantity > 0:
             basket[item_id] = quantity
+            messages.success(request, f'Updated {product.name} quantity to {basket[item_id]}')
+
         else:
             basket.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your basket')
+
 
     request.session['basket'] = basket
     return redirect(reverse('view_basket'))
@@ -79,6 +88,7 @@ def remove_from_basket(request, item_id):
     """Remove the item from the shopping basket """
 
     try:
+        product = get_object_or_404(Product,(pk=item_id)
         size = None
         if 'product_size' in request.POST:
             size = request.POST['product_size']
@@ -88,12 +98,15 @@ def remove_from_basket(request, item_id):
             del basket[item_id]['items_by_size'][size]
             if not basket[item_id]['items_by_size']:
                 basket.pop(item_id)
+            messages.success(request, f'Removed size {size.upper()} {Product.name} from your basket')
 
         else:
             basket.pop(item_id)
+            messages.success(request, f'Removed {Product.name} from your basket')
 
         request.session['basket'] = basket
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(requst, f'Error removing item: (e)')
         return HttpResponse(status=500)
