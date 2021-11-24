84b63e148917a96cecad02bd6e722358f2f039fd
Leah1977 <lh020277@gmail.com>
2021-11-24 22:49:44 +0000

    Add OrderAdmin class

--- checkout/admin.py
+++ checkout/admin.py
@@ -5,13 +5,13 @@ from .models import Order, OrderLineItem
 
 
 class OrderLineItemAdminInline(admin.TabularInline):
-    model - OrderLineItem
+    model = OrderLineItem
     readonly_fields = ('lineitem_total',)
 
 
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('order_number', 'date',
-                       'delivery_cost', 'ordre_total',
+                       'delivery_cost', 'order_total',
                        'grand_total',)
 
     fields = ('order_number', 'date', 'full_name',
