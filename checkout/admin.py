1d20b487bf16bd82179d5d8f9bdb840166c7868a
Leah1977 <lh020277@gmail.com>
2021-11-27 17:34:18 +0000

    Update OrderAdmin Class

--- checkout/admin.py
+++ checkout/admin.py
@@ -11,17 +11,17 @@ class OrderLineItemAdminInline(admin.TabularInline):
 
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('order_number', 'date',
-                       'delivery_cost', 'order_total',
+                       'shipping_cost', 'order_total',
                        'grand_total', 'original_basket', 'stripe_pid')
 
     fields = ('order_number', 'date', 'full_name',
               'email', 'phone_number', 'country',
               'postcode', 'town_or_city', 'street_address1',
-              'street_address2', 'county', 'delivery_cost',
+              'street_address2', 'county', 'shipping_cost',
               'order_total', 'grand_total', 'original_basket', 'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name',
-                    'order_total', 'delivery_cost',
+                    'order_total', 'shipping_cost',
                     'grand_total',)
 
     ordering = ('-date',)
