5e8954dac64bddbd4c22502cec986fc516272bff
Leah1977 <lh020277@gmail.com>
2021-11-30 20:27:52 +0000

    Edit views for line length

--- products/views.py
+++ products/views.py
@@ -37,7 +37,7 @@ def all_products(request):
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
-     
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -58,6 +58,7 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
 
 # A view to render the product detail template
 def product_detail(request, product_id):
@@ -87,8 +88,9 @@ def add_product(request):
             messages.success(request, "Successfully added product!")
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid')
-    else: 
+            messages.error(request, 'Failed to add product. \
+                Please ensure the form is valid')
+    else:
         form = ProductForm()
 
     template = 'products/add_product.html'
@@ -115,8 +117,9 @@ def edit_product(request, product_id):
             messages.success(request, "Successfully updated product!")
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to edit product. Please ensure the form is valid')
-    else: 
+            messages.error(request, 'Failed to edit product. \
+                Please ensure the form is valid')
+    else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
 
@@ -128,13 +131,14 @@ def edit_product(request, product_id):
 
     return render(request, template, context)
 
+
 @login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
     if not request.user.is_superuser:
         messages.error(request, 'Sorry, For management only.')
         return redirect(reverse('home'))
-        
+
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
