7869e187a58e96fd512bc3f5d3264007700fec73
Leah1977 <lh020277@gmail.com>
2021-12-05 00:23:50 +0000

    Add url links for add review

--- reviews/urls.py
+++ reviews/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add_review/<product.id>', views.add_review, name="add_review"),
-    
+     path('add_review/<product_id>', views.add_review, name='add_review'),
+     path('edit_review/<int:review_id>/',
+          views.edit_review, name='edit_review'),
+     path('delete_review/<int:review_id>/',
+          views.edit_review, name='delete_review'),
 ]
