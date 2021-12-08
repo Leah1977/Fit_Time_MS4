af6210ba631c1d3eff316b67cf9262a52e87db99
Leah1977 <lh020277@gmail.com>
2021-12-08 08:44:39 +0000

    Add class contact
    Add details for name, title, messages and created at

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    """ allow a user to Contact us with a message """
+
+    name = models.CharField(max_length=254)
+    title = models.CharField(max_length=254)
+    message = models.TextField(default=0, blank=True, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.title)
