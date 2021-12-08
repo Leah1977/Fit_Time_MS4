b1c79a589e56cff893a8fdb9a3c5cf20673010db
Leah1977 <lh020277@gmail.com>
2021-12-08 10:06:20 +0000

    Add oontact form

---
+++ contact/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from .models import Contact
+
+
+class ContactForm(forms.ModelForm):
+    """
+    Create a form to contact us
+    """
+    class Meta:
+        model = Contact
+        fields = ['name', 'title', 'message', 'email' ]
+
+    def __init__(self, *args, **kwargs):
+        """ Insert placeholders for form """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Name'
+            'title': 'Title',
+            'message': 'Message',
+            'email': 'Email',
+        }
