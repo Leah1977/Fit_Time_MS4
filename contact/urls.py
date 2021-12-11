d5fcb2b0b2d5001d2ca8728cd2be3b6a71a60f42
Leah1977 <lh020277@gmail.com>
2021-12-11 20:39:34 +0000

    Add view for contact form

--- contact/urls.py
+++ contact/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.contact, name='contact'),
+    path('add/', views.add_contact_message, name='add_contact_message'),
 ]
--- contact/views.py
+++ contact/views.py
@@ -5,3 +5,24 @@ def contact(request):
     """ A view to return the contact page """
 
     return render(request, 'contact/contact.html')
+
+def add_contact_message(request):
+    """ Create a message to send to store owner  """
+    if request.method == 'POST':
+        form = ContactForm(request.POST, request.FILES)
+        if form.is_valid():
+            message = form.save()
+            messages.success(request, "Successfully added message!")
+            return redirect(reverse('home'))
+        else:
+            messages.error(request, 'Failed to add message. \
+                Please ensure the form is valid')
+    else:
+        form = ContactForm()
+
+    template = 'contact/contact.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
