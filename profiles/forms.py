4ddd468709e3850b92949bcb7bf72380321015a1
Leah1977 <lh020277@gmail.com>
2021-12-11 11:36:30 +0000

    Correct indentation errors

--- profiles/forms.py
+++ profiles/forms.py
@@ -30,7 +30,7 @@ class UserProfileForm(forms.ModelForm):
                     placeholder = f'{placeholders[field]} *'
                 else:
                     placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-        self.fields[field].widget.attrs['class'] =
-        'border-black rounded-1 profile-form-input'
-        self.fields[field].label = False
+                self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs[
+                'class'] = 'border-black rounded-1 profile-form-input'
+            self.fields[field].label = False
