7de2431d9fd7e590c6bde95aee9f871d4f0c852b
Leah1977 <lh020277@gmail.com>
2021-12-12 17:33:20 +0000

    Edit styling for contact us page

--- contact/urls.py
+++ contact/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.contact, name='contact'),
-    path('add_contact_message.html', 
-         views.add_contact_message, name='add_contact'),
+    path(
+        'add_contact_message.html', views.add_contact_message,
+        name='add_contact_message'),
 ]
